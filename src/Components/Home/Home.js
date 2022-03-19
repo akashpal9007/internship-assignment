5f7ab227dba47fd0c052305670df5721406882d7
Akash <akashpal9007@gmail.com>
2022-03-19 20:57:07 +0530

    updated app.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,7 +8,9 @@ const Home = () => {
         <h1>Abstract Gradient LandingPage Template</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
+    <div>
         <button className='jbtn'>JOIN NOW</button>
+        </div>
     </>
   )
 }
